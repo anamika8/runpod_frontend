bcbb2d21d938c39de6b6574ca4820b269844818d
Anamika Nayak <40330477+anamika8@users.noreply.github.com>
2025-01-30 01:27:43 +0000

    adding delay function

--- src/services/runpodService.js
+++ src/services/runpodService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-import home_gif from '../assets/home.gif'
+import home_gif from '../assets/home.gif';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const DEFAULT_IMAGE_TO_SHOW = home_gif;
 const RUNPOD_HOST_URL = `https://api.runpod.ai/v2/t66yn9dzz7jydx/run`;
+const STATUS_URL = `https://api.runpod.ai/v2/t66yn9dzz7jydx/status`; 
 const DEFAULT_SEARCH_TEXT = `an astronaut riding a horse, digital art, epic lighting, highly-detailed masterpiece trending HQ`;
 
 // public function that can be called by the UI or other services
@@ -35,7 +36,7 @@ export async function callRunpodApiWithSearchText(searchText = "") {
 
         while (retries > 0) {
             console.log(`Checking job status for job ID: ${jobId}`);
-            jobStatusResponse = await axios.get(`${STATUS_URL}${jobId}`, { headers });
+            jobStatusResponse = await axios.get(`${STATUS_URL}/${jobId}`, { headers });
             console.log(jobStatusResponse);
             if (jobStatusResponse.status === 'COMPLETED') {
                 console.log("Job completed successfully.");
@@ -60,4 +61,8 @@ export async function callRunpodApiWithSearchText(searchText = "") {
         console.error('Caught Error:', error);
         return DEFAULT_IMAGE_TO_SHOW;
     }
+}
+
+async function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }

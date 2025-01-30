aa949f01d80ff67cce5ed5440377df159a88c7a8
Anamika Nayak <40330477+anamika8@users.noreply.github.com>
2025-01-30 00:53:14 +0000

    fixing header for auth key

--- src/services/runpodService.js
+++ src/services/runpodService.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 import home_gif from '../assets/home.gif'
 
-const RUNPOD_API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 const DEFAULT_IMAGE_TO_SHOW = home_gif;
 const RUNPOD_HOST_URL = `https://api.runpod.ai/v2/t66yn9dzz7jydx/run`;
 const DEFAULT_SEARCH_TEXT = `an astronaut riding a horse, digital art, epic lighting, highly-detailed masterpiece trending HQ`;
 
 // public function that can be called by the UI or other services
 export async function callRunpodApiWithSearchText(searchText = "") {
-    console.log(`Hitting the URL ${RUNPOD_HOST_URL} and API key - ${RUNPOD_API_KEY}`)
+    console.log(`Hitting the URL ${RUNPOD_HOST_URL} and API key - ${API_KEY}`)
     if (searchText === "") {
         searchText = DEFAULT_SEARCH_TEXT;
     }
@@ -20,7 +20,7 @@ export async function callRunpodApiWithSearchText(searchText = "") {
 
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': RUNPOD_API_KEY,
+        'Authorization': `Bearer ${API_KEY}`,
     };
     let imageBase64 = undefined;
     axios.post(RUNPOD_HOST_URL, data, { headers })

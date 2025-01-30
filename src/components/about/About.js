a23e97b09dea34f030161818cf98a81be7591fe2
Anamika Nayak <40330477+anamika8@users.noreply.github.com>
2025-01-30 06:55:58 +0000

    Fixed about section of the page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,18 +6,11 @@ function About() {
         <div className="about-container">
             <div className="about-content">
                 <p>
-                    Introducing Asciiart, our web app utilizes Dezgo's text-to-image API to generate images, which can then be converted into ASCII art.
+                    Introducing a web app utilizes Stability AI API to generate images, based on text input.
                 </p>
                 <p>
-                    Our responsive and user-friendly interface allows for easy input of text that will run through a stable diffusion model to generate a image.  
-                    <a id='info-link' href='https://en.wikipedia.org/wiki/Stable_Diffusion'>Click here</a> to learn more about stable diffusion!
+                    Give it a try and watch as your words come to life in the most unique and visually appealing way possible!
                 </p>
-                <p>
-                    Give Asciiart a try and watch as your words come to life in the most unique and visually appealing way possible!
-                </p>
-            </div>
-            <div className='about-footer'>
-                <Title asciiArt={art} />
             </div>
         </div>
     )

6b5546da050cb021fd40efda45d6e4022cc0b27a
Anamika Nayak <40330477+anamika8@users.noreply.github.com>
2025-01-30 15:52:41 -0800

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,7 +6,8 @@ function About() {
         <div className="about-container">
             <div className="about-content">
                 <p>
-                    Introducing a web app utilizes Stability AI API to generate images, based on text input.
+                    Introducing a web app that internally calls a Serverless Endpoint hosted on the RunPod platform.
+                    The serverless endpoint used `Stability AI` model to generate images, based on text input.
                 </p>
                 <p>
                     Give it a try and watch as your words come to life in the most unique and visually appealing way possible!
